cd8c2a0ac15031bb5183732468484af72b7ac757
Mitchell Cash <mitchell.cash@gmail.com>
2016-04-25 16:28:53 +1000

    Cleanup Paycoin params

--- params/paycoin.go
+++ params/paycoin.go
@@ -6,6 +6,6 @@ import "github.com/btcsuite/btcd/chaincfg"
 var PaycoinParams = chaincfg.Params{
 	Name: "paycoin",
 
-	PubKeyHashAddrID: 0x37,               // starts with P
-	PrivateKeyID:     0x99 + 0x19 + 0x05, // hacky?
+	PubKeyHashAddrID: 0x37, // starts with P
+	PrivateKeyID:     0xB7, // starts with 7 (uncompressed) or U (compressed)
 }
